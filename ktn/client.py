add928c12e8b9bfeb157c136c16d7435676fe806
Erik Bugge <embugge@hotmail.com>
2014-02-21 15:34:40 +0100

    Client changes

--- ktn/client.py
+++ ktn/client.py
@@ -2,6 +2,7 @@
 KTN-project 2013 / 2014
 '''
 import socket
+import json
 
 
 class Client(object):
@@ -11,13 +12,32 @@ class Client(object):
 
     def start(self, host, port):
         self.connection.connect((host, port))
-        self.send('Hello')
+        
+        
+
+        self.send(nick + ' has logged on')
         received_data = self.connection.recv(1024).strip()
         print 'Received from server: ' + received_data
-        self.connection.close()
+
+
+        while True:
+            message = raw_input('Enter a message (type logout to quit)')
+
+            if (message == 'logout'):
+                data = ('nick': nick, 'request': 'logout')
+            else:
+                data = ('request': 'message', 'message': message)
+
+            data = json.dumps(data)
+            self.send(data)
+            self.message_received('Received from server: ', connection)
+
+        connection_closed(connection)
 
     def message_received(self, message, connection):
-        print message
+        received_data = self.connection.recv(1024)
+        print message + received_data
+
 
     def connection_closed(self, connection):
         self.connection.close()
@@ -26,8 +46,26 @@ class Client(object):
         self.connection.sendall(data)
 
     def force_disconnect(self):
-        pass
+        self.shutdown()
 
+    def loginCheck(self):
+        check = True
+        while check:
+            nick = raw_input('Enter your username: ')
+            loginData = ('request': 'login', 'username': nick)
+            loginData = json.dumps(data)
+            self.send(loginData)
+            message = self.message_received('Received from server: ', connection)
+            response = json.loads(message)
+            if(response.get('error') == 'Invalid username!'):
+                check = True
+            elif(response.get('error') == 'Name allready taken!'):
+                check = True
+            else:
+                check = False
+        return nick
+
+    def 
 
 if __name__ == "__main__":
     client = Client()
