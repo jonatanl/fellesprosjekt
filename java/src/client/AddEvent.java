fa5f18f669c5ab387cb3baa44d6bc2ed1fbcf934
Jonatan Lund <jonatan.lund.92@gmail.com>
2014-02-26 14:10:13 +0100

    Created login and add event screen. Added models

---
+++ java/src/client/AddEvent.java
@@ -0,0 +1,89 @@
+package client;
+
+import Models.Event;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class AddEvent extends Application implements EventHandler<ActionEvent> {
+    private TextField titleField;
+    private TextField startTime;
+    private TextField endTime;
+    private TextField description;
+    private TextField location;
+
+    private Event model;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text title = new Text("Event");
+        title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(title, 0, 0, 2, 1);
+
+        createFields(grid);
+        createLabels(grid);
+
+        Button button = new Button("Add event");
+        grid.add(button, 1, 6);
+
+        Scene scene = new Scene(grid, 300, 275);
+        stage.setScene(scene);
+        stage.show();
+
+        button.setOnAction(this);
+        model = new Event();
+    }
+
+    private void createLabels(GridPane grid){
+        grid.add(new Label("Title"), 0, 1);
+        grid.add(new Label("Start Time"), 0, 2);
+        grid.add(new Label("End Time"), 0, 3);
+        grid.add(new Label("Description"), 0, 4);
+        grid.add(new Label("Location"), 0, 5);
+    }
+
+    private void createFields(GridPane grid) {
+        titleField = new TextField();
+        startTime = new TextField();
+        endTime = new TextField();
+        description = new TextField();
+        location = new TextField();
+
+        grid.add(titleField, 1, 1);
+        grid.add(startTime, 1, 2);
+        grid.add(endTime, 1, 3);
+        grid.add(description, 1, 4);
+        grid.add(location, 1, 5);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        model.setTitle(titleField.getText());
+        model.setStartTime(startTime.getText());
+        model.setEndTime(endTime.getText());
+        model.setDescription(description.getText());
+        model.setLocation(location.getText());
+        System.out.println(model);
+    }
+}
---
+++ java/src/client/Login.java
@@ -0,0 +1,78 @@
+package client;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Login extends Application implements EventHandler<ActionEvent> {
+    private Text messageText;
+    private TextField username;
+    private PasswordField password;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("YO!");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Username:");
+        grid.add(userName, 0, 1);
+
+        username = new TextField();
+        grid.add(username, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        password = new PasswordField();
+        grid.add(password, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        messageText = new Text("Sign in button pressed");
+        messageText.setFill(Color.FIREBRICK);
+        messageText.setVisible(false);
+        grid.add(messageText, 1, 6);
+
+        Scene scene = new Scene(grid, 300, 275);
+        stage.setScene(scene);
+        stage.show();
+
+        btn.setOnAction(this);
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if (!messageText.isVisible())
+            messageText.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
