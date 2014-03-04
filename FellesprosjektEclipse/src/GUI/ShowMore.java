7fe7e948c1f937a12a522607f3d72114a08bc337
simejo <simen.johnsrud92@gmail.com>
2014-03-04 13:52:02 +0100

    Added GUI package

---
+++ FellesprosjektEclipse/src/GUI/ShowMore.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class ShowMore {
+	
+	private Scene scene;
+	private Stage stage;
+	
+	private Text title, date, start,end, description, place, room, alert, timeBefore, response;
+}
+
