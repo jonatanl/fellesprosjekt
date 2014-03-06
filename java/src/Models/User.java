a2144dd2bc09957b39322ea20250758da55b6523
ofossan <oivind_fossan@hotmail.com>
2014-03-06 12:57:45 +0100

    Added fields and methods to Calendar, created class User

--- java/src/Models/User.java
+++ java/src/Models/User.java
@@ -1,4 +1,26 @@
 package Models;
 
 public class User{
+	private int userId;
+	private String username;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
 }
--- java/src/client/Calendar.java
+++ java/src/client/Calendar.java
@@ -1,8 +1,13 @@
 package client;
 
+import interfaces.CalendarInterface;
+
 import java.util.ArrayList;
 
+import Models.Alarm;
 import Models.Event;
+import Models.EventParticipant;
+import Models.Group;
 import Models.User;
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -14,10 +19,11 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 //Main class of the calendar system. 
-public class Calendar extends Application{
+public class Calendar extends Application implements CalendarInterface{
 	
 	private ArrayList<Event> events;
 	private ArrayList<User> users;
+	private ArrayList<Group> groups;
 	
 	public static void main(String[] args)  {
 		launch(args);
@@ -37,4 +43,73 @@ public class Calendar extends Application{
         stage.setScene(scene);
         stage.show();
 	}
+
+	@Override
+	public void addEvent(Event event) {
+		events.add(event);
+		// TODO Update the webView. 
+	}
+
+	@Override
+	public void removeEvent(Event event) {
+		events.remove(event);
+		// TODO Update the webView.
+	}
+
+	@Override
+	public void changeEvent(Event event) {
+		
+	}
+	
+	public Event findEvent(int eventID){
+		for (Event e: events){
+			if (e.getEventId() == eventID){
+				return e;
+			}
+		}
+		return null;
+	}
+
+	public User findUser(int userID){
+		
+	}
+	
+	@Override
+	public void addEventParticipant(int eventID, EventParticipant participant) {
+		Event e = findEvent(eventID);
+		if (e != null){
+			e.addEventParticipant(participant);
+		}
+	}
+
+	@Override
+	public void removeEventParticipant(int eventID, EventParticipant participant) {
+				
+	}
+
+	@Override
+	public void changeEventParticipantResponse(EventParticipant participant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAlarm(EventParticipant participant, Alarm alarm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeAlarm(EventParticipant participant, Alarm alarm) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void changeAlarm(EventParticipant participant, Alarm alarm) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
