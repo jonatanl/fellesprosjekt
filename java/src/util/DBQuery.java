c6ce64c13d0184ccdd5c78f905f4e74350dd7e69
Jonatan Lund <jonatan.lund.92@gmail.com>
2014-03-11 13:19:38 +0100

    Fixed changeEvent error

--- java/src/util/DBQuery.java
+++ java/src/util/DBQuery.java
@@ -36,7 +36,13 @@ public class DBQuery extends DBQueryGetMethods {
         String query = "UPDATE event set eventName=?, endTime=?, startTime=?, description=?, location=?, roomID=?, ownerID=?"
                 + "WHERE eventID=" + event.getEventId();
         PreparedStatement statement = connection.prepareStatement(query);
-        setEventFields(statement, event);
+        statement.setString(1, event.getEventName());
+        statement.setString(2, new java.sql.Date(event.getEndTime().getTime()).toString());
+        statement.setString(3, new java.sql.Date(event.getStartTime().getTime()).toString());
+        statement.setString(4, event.getDescription());
+        statement.setString(5, event.getLocation());
+        statement.setString(6, event.getRoomId() + "");
+        statement.setString(7, event.getOwnerId() + "");
         statement.executeUpdate();
     }
 
@@ -55,8 +61,8 @@ public class DBQuery extends DBQueryGetMethods {
 
     private void setEventFields (PreparedStatement statement, Event event) throws SQLException{
         statement.setString(1, event.getEventName());
-        statement.setString(2, DateHelper.convertToString(event.getStartTime(),DateHelper.FORMAT_GUI));
-        statement.setString(3, DateHelper.convertToString(event.getEndTime(),DateHelper.FORMAT_GUI));
+        statement.setString(2, DateHelper.convertToString(event.getStartTime(), DateHelper.FORMAT_GUI));
+        statement.setString(3, DateHelper.convertToString(event.getEndTime(), DateHelper.FORMAT_GUI));
         statement.setString(4, event.getDescription());
         statement.setString(5, event.getLocation());
         statement.setString(6, event.getRoomId() + "");
