1f3bcdecf8bdc2c3d16f0dc575630042da85e911
ofossan <oivind_fossan@hotmail.com>
2014-03-14 13:49:56 +0100

    Added logout button.

--- java/src/client/Buttons.java
+++ java/src/client/Buttons.java
@@ -12,11 +12,14 @@ import Models.EventParticipant;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class Buttons extends VBox implements EventHandler<ActionEvent> {
-	private Button b_createEvent, b_editEvent, b_deleteEvent, b_showMore, b_alarm, b_test;
+	private Button b_createEvent, b_editEvent, b_deleteEvent, b_showMore, b_alarm, b_test, b_logout;
+	private Label extraSpaceText1, extraSpaceText2;
 	private Calendar calendar;
 	private boolean isOwner = false;
 	private Event selectedEvent = null;
@@ -49,6 +52,9 @@ public class Buttons extends VBox implements EventHandler<ActionEvent> {
 	public Buttons(Calendar calendar){
 		this.calendar = calendar;
 		
+		extraSpaceText1 = new Label();
+		extraSpaceText1.setMinHeight(50);
+		
 		// Create the buttons.
 		b_createEvent = new Button("Add event");
 		b_createEvent.setMinWidth(80);
@@ -80,7 +86,15 @@ public class Buttons extends VBox implements EventHandler<ActionEvent> {
 		b_test.setMinWidth(80);
 		b_test.setMinHeight(30);
 		
-		this.getChildren().addAll(b_createEvent, b_editEvent, b_deleteEvent, b_showMore, b_alarm, b_test);
+		extraSpaceText2 = new Label();
+		extraSpaceText2.setMinHeight(50);
+		
+		b_logout = new Button("Log out");
+		b_logout.setOnAction(this);
+		b_logout.setMinWidth(80);
+		b_logout.setMinHeight(30);
+		
+		this.getChildren().addAll(extraSpaceText1, b_createEvent, b_editEvent, b_deleteEvent, b_showMore, b_alarm, b_test, extraSpaceText2, b_logout);
 		this.setSpacing(10);
 		
 		setSelectedEvent(null);
@@ -135,6 +149,13 @@ public class Buttons extends VBox implements EventHandler<ActionEvent> {
 			for (Alarm a: calendar.getAlarms()){
 				System.out.println(a.getAlarmID() + " " + a.getTime());
 			}
+		}
+		
+		else if (buttonEvent.getSource() == b_logout){
+			calendar.getStage().close();
+			
+			Login login = new Login(calendar, calendar.getPersistency());
+			login.createStage();
 		}
 	}
 }
