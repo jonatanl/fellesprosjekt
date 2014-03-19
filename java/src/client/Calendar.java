7b7c643730e47254b8d8dc47d0f3ba09bbd5c572
ochamp <ole.selvig@gmail.com>
2014-03-19 10:22:13 +0100

    png for event descripton added to rep, might change some of the colors; pink. lol

--- java/src/client/Calendar.java
+++ java/src/client/Calendar.java
@@ -269,6 +269,7 @@ public class Calendar extends CalendarLists {
 				persistency.changeEventParticipantResponse(epOther);
 			}
 		}
+        updateWebScene();
 	}
 	
 	// Draws the webScene over again. 
