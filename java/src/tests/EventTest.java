ee442ede94092389d903c8ae12a101bd4c33d2c2
Jonatan Lund <jonatan.lund.92@gmail.com>
2014-03-19 13:26:00 +0100

    Finished with event tests

--- java/src/tests/EventTest.java
+++ java/src/tests/EventTest.java
@@ -6,14 +6,42 @@ import junit.framework.TestCase;
 import util.Persistency;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class EventTest extends TestCase{
 
     public void testAddEvent() throws Exception {
         PersistencyInterface persistency = new Persistency();
-        Event event = new Event();
+
+        Event event = addEvent();
         ArrayList<Integer> participantIDs = new ArrayList<>();
+        participantIDs.add(1);
         boolean success = persistency.addEvent(event,participantIDs);
         assertTrue("Added event", success);
+    }
+
+
+    public void testRemoveEvent() throws Exception {
+        PersistencyInterface persistency = new Persistency();
+        Event event = addEvent();
+        ArrayList<Integer> participantIDs = new ArrayList<>();
+        participantIDs.add(1);
+        persistency.addEvent(event, participantIDs);
+
+        boolean success = persistency.removeEvent(event);
+        assertTrue(success);
+    }
+
+    private Event addEvent() {
+        Event event = new Event();
+        event.setEventName("Test");
+        event.setStartTime(new Date());
+        event.setEndTime(new Date());
+        event.setDescription("TestDescription");
+        event.setOwnerId(1);
+        event.setRoomId(2);
+        event.setLocation("");
+
+        return event;
     }
 }
